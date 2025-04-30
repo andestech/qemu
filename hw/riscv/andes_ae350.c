andestech/qemu

commit 871bacac05ceed70d3e814ff1c370c62c68ccff4

hw/riscv: Load dtb to ae350 DTROM region. (#214)
Signed-off-by: Yu-Ming Chang <yumin686@andestech.com>
Reviewed-on: https://gitea.andestech.com/RD-SW/qemu/pulls/214
Reviewed-by: Ethan Zhan-Yang Chen <ethan84@andestech.com>
Reviewed-by: Mina Hui-Min Chou <minachou@andestech.com>
Co-authored-by: Yu-Ming Chang <yumin686@andestech.com>
Co-committed-by: Yu-Ming Chang <yumin686@andestech.com>

hw/riscv/andes_ae350.c
@@ -123,6 +123,7 @@ static const struct MemmapEntry {
     [ANDES_AE350_IOPMP_DLMAP]       = { 0xf101c000,       0x4000 },
     [ANDES_AE350_IOPMP100]          = { 0xf1024000,       0x4000 },
     [ANDES_AE350_UART3]             = { 0xf1100000,     0x100000 },
+    [ANDES_AE350_DTROM]             = { 0xf2000000,     0x100000 },
     [ANDES_AE350_VIRTIO]            = { 0xfe000000,       0x1000 },
     [ANDES_AE350_UNCACHEABLE_ALIAS] = { 0x100000000, 0x100000000 },
 };
@@ -1322,9 +1323,9 @@ static void andes_ae350_machine_init(MachineState *machine)
     MemoryRegion *mask_nor = g_new(MemoryRegion, 1);
     MemoryRegion *mask_hvm = g_new(MemoryRegion, 1);
     MemoryRegion *mask_l2c = g_new(MemoryRegion, 1);
+    MemoryRegion *mask_drom = g_new(MemoryRegion, 1);
     target_ulong start_addr = memmap[ANDES_AE350_DRAM].base;
     target_ulong firmware_end_addr, kernel_start_addr;
-    uint32_t fdt_load_addr;
     uint64_t kernel_entry;
 
     /* Initialize SoC */
@@ -1421,17 +1422,18 @@ static void andes_ae350_machine_init(MachineState *machine)
         kernel_entry = 0;
     }
 
-    /* Compute the fdt load address in dram */
-    fdt_load_addr = riscv_compute_fdt_addr(memmap[ANDES_AE350_DRAM].base,
-                                           memmap[ANDES_AE350_DRAM].size,
-                                           machine);
-    riscv_load_fdt(fdt_load_addr, machine->fdt);
+    /* dtb rom */
+    memory_region_init_rom(mask_drom, NULL, "riscv.andes.ae350.dtrom",
+                           memmap[ANDES_AE350_DTROM].size, &error_fatal);
+    memory_region_add_subregion(system_memory, memmap[ANDES_AE350_DTROM].base,
+                                mask_drom);
+    riscv_load_fdt(memmap[ANDES_AE350_DTROM].base, machine->fdt);
 
     /* load the reset vector */
     riscv_setup_rom_reset_vec(machine, &bs->soc.cpus, start_addr,
-                andes_ae350_memmap[ANDES_AE350_MROM].base,
-                andes_ae350_memmap[ANDES_AE350_MROM].size,
-                kernel_entry, fdt_load_addr);
+                              memmap[ANDES_AE350_MROM].base,
+                              memmap[ANDES_AE350_MROM].size,
+                              kernel_entry, memmap[ANDES_AE350_DTROM].base);
 
     if (bs->soc.has_iopmp100) {
         iopmp100_setup_system_memory_range(bs->soc.iopmp100_dev,

include/hw/riscv/andes_ae350.h
@@ -161,6 +161,7 @@ enum {
     ANDES_AE350_IOPMP_DLMAP,
     ANDES_AE350_IOPMP100,
     ANDES_AE350_UART3,
+    ANDES_AE350_DTROM,
     ANDES_AE350_VIRTIO,
     ANDES_AE350_UNCACHEABLE_ALIAS,
 };

commit d424503089c5ef5427e5619fb644f998cc6a247d

hw/riscv: Fix a segmentation fault when resetting. (#217)
This segmantation fault is caused by #214.

Signed-off-by: Yu-Ming Chang <yumin686@andestech.com>

Reviewed-on: https://gitea.andestech.com/RD-SW/qemu/pulls/217
Reviewed-by: Mina Hui-Min Chou <minachou@andestech.com>
Reviewed-by: Ethan Zhan-Yang Chen <ethan84@andestech.com>
Co-authored-by: Yu-Ming Chang <yumin686@andestech.com>
Co-committed-by: Yu-Ming Chang <yumin686@andestech.com>

hw/riscv/andes_ae350.c
@@ -1324,7 +1324,7 @@ static void andes_ae350_machine_init(MachineState *machine)
     MemoryRegion *mask_nor = g_new(MemoryRegion, 1);
     MemoryRegion *mask_hvm = g_new(MemoryRegion, 1);
     MemoryRegion *mask_l2c = g_new(MemoryRegion, 1);
-    MemoryRegion *mask_drom = g_new(MemoryRegion, 1);
+    MemoryRegion *mask_dtrom = g_new(MemoryRegion, 1);
     target_ulong start_addr = memmap[ANDES_AE350_DRAM].base;
     target_ulong firmware_end_addr, kernel_start_addr;
     uint64_t kernel_entry;
@@ -1424,11 +1424,11 @@ static void andes_ae350_machine_init(MachineState *machine)
     }
 
     /* dtb rom */
-    memory_region_init_rom(mask_drom, NULL, "riscv.andes.ae350.dtrom",
+    memory_region_init_rom(mask_dtrom, NULL, "riscv.andes.ae350.dtrom",
                            memmap[ANDES_AE350_DTROM].size, &error_fatal);
     memory_region_add_subregion(system_memory, memmap[ANDES_AE350_DTROM].base,
-                                mask_drom);
-    riscv_load_fdt(memmap[ANDES_AE350_DTROM].base, machine->fdt);
+                                mask_dtrom);
+    riscv_load_fdt(memmap[ANDES_AE350_DTROM].base, machine->fdt, true);
 
     /* load the reset vector */
     riscv_setup_rom_reset_vec(machine, &bs->soc.cpus, start_addr,

hw/riscv/boot.c
@@ -330,7 +330,7 @@ uint64_t riscv_compute_fdt_addr(hwaddr dram_base, hwaddr dram_size,
  * 'fdt_addr' is received as hwaddr because boards might put
  * the FDT beyond 32-bit addressing boundary.
  */
-void riscv_load_fdt(hwaddr fdt_addr, void *fdt)
+void riscv_load_fdt(hwaddr fdt_addr, void *fdt, bool is_load_to_rom)
 {
     uint32_t fdtsize = fdt_totalsize(fdt);
 
@@ -339,8 +339,20 @@ void riscv_load_fdt(hwaddr fdt_addr, void *fdt)
 
     rom_add_blob_fixed_as("fdt", fdt, fdtsize, fdt_addr,
                           &address_space_memory);
-    qemu_register_reset_nosnapshotload(qemu_fdt_randomize_seeds,
-                        rom_ptr_for_as(&address_space_memory, fdt_addr, fdtsize));
+
+    /*
+     * If the FDT is write to a read-only memory region(ROM), the data of
+     * ROM blob will be freed because we only need to write the FDT to the
+     * read-only memory region once(see function rom_reset()).
+     * To avoid a segmentation fault caused by the registered function
+     * qemu_fdt_randomize_seeds() when resetting, and since we actually cannot
+     * randomize seeds of the FDT in the ROM, so we do not register this
+     * function if the FDT is loaded to the ROM.
+     */
+     if (!is_load_to_rom) {
+        qemu_register_reset_nosnapshotload(qemu_fdt_randomize_seeds,
+                    rom_ptr_for_as(&address_space_memory, fdt_addr, fdtsize));
+     }
 }
 
 void riscv_rom_copy_firmware_info(MachineState *machine,

hw/riscv/microchip_pfsoc.c
@@ -626,7 +626,7 @@ static void microchip_icicle_kit_machine_init(MachineState *machine)
         fdt_load_addr = riscv_compute_fdt_addr(memmap[MICROCHIP_PFSOC_DRAM_LO].base,
                                                memmap[MICROCHIP_PFSOC_DRAM_LO].size,
                                                machine);
-        riscv_load_fdt(fdt_load_addr, machine->fdt);
+        riscv_load_fdt(fdt_load_addr, machine->fdt, false);
 
         /* Load the reset vector */
         riscv_setup_rom_reset_vec(machine, &s->soc.u_cpus, firmware_load_addr,

hw/riscv/sifive_u.c
@@ -607,7 +607,7 @@ static void sifive_u_machine_init(MachineState *machine)
     fdt_load_addr = riscv_compute_fdt_addr(memmap[SIFIVE_U_DEV_DRAM].base,
                                            memmap[SIFIVE_U_DEV_DRAM].size,
                                            machine);
-    riscv_load_fdt(fdt_load_addr, machine->fdt);
+    riscv_load_fdt(fdt_load_addr, machine->fdt, false);
 
     if (!riscv_is_32bit(&s->soc.u_cpus)) {
         start_addr_hi32 = (uint64_t)start_addr >> 32;

hw/riscv/spike.c
@@ -318,7 +318,7 @@ static void spike_board_init(MachineState *machine)
     fdt_load_addr = riscv_compute_fdt_addr(memmap[SPIKE_DRAM].base,
                                            memmap[SPIKE_DRAM].size,
                                            machine);
-    riscv_load_fdt(fdt_load_addr, machine->fdt);
+    riscv_load_fdt(fdt_load_addr, machine->fdt, false);
 
     /* load the reset vector */
     riscv_setup_rom_reset_vec(machine, &s->soc[0], firmware_load_addr,

hw/riscv/virt.c
@@ -1425,7 +1425,7 @@ static void virt_machine_done(Notifier *notifier, void *data)
     fdt_load_addr = riscv_compute_fdt_addr(memmap[VIRT_DRAM].base,
                                            memmap[VIRT_DRAM].size,
                                            machine);
-    riscv_load_fdt(fdt_load_addr, machine->fdt);
+    riscv_load_fdt(fdt_load_addr, machine->fdt, false);
 
     /* load the reset vector */
     riscv_setup_rom_reset_vec(machine, &s->soc[0], start_addr,

include/hw/riscv/boot.h
@@ -50,7 +50,7 @@ target_ulong riscv_load_kernel(MachineState *machine,
                                symbol_fn_t sym_cb);
 uint64_t riscv_compute_fdt_addr(hwaddr dram_start, uint64_t dram_size,
                                 MachineState *ms);
-void riscv_load_fdt(hwaddr fdt_addr, void *fdt);
+void riscv_load_fdt(hwaddr fdt_addr, void *fdt, bool is_load_to_rom);
 void riscv_setup_rom_reset_vec(MachineState *machine, RISCVHartArrayState *harts,
                                hwaddr saddr,
                                hwaddr rom_base, hwaddr rom_size,