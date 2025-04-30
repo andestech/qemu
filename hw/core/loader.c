e8a7097660100a1c1fb9c3808d18685191bb2434
Yu-Ming Chang <yumin686@andestech.com>
2025-04-30 16:07:04 +0800

    hw/core: Fix the order of the roms list. (#219)
    The original logic for comparing the order involves directly comparing pointers,
    which may lead to an unintended order of the roms list.
    For exmple, using a generic loader to load a file into memory in Windows may
    failed because the content is overwritten by other roms.
    
    We change the logic as follows:
    1. If pointers of AddressSpace are different, return greater.
    2. If pointers of AddressSpace are the same, return greater if the address
       is greater.
    
    Signed-off-by: Yu-Ming Chang <yumin686@andestech.com>
    
    Reviewed-on: https://gitea.andestech.com/RD-SW/qemu/pulls/219
    Reviewed-by: Alvin Che-Chia Chang <alvinga@andestech.com>
    Co-authored-by: Yu-Ming Chang <yumin686@andestech.com>
    Co-committed-by: Yu-Ming Chang <yumin686@andestech.com>

--- hw/core/loader.c
+++ hw/core/loader.c
@@ -1001,7 +1001,7 @@ static void rom_free(Rom *rom)
 
 static inline bool rom_order_compare(Rom *rom, Rom *item)
 {
-    return ((uintptr_t)(void *)rom->as > (uintptr_t)(void *)item->as) ||
+    return ((uintptr_t)(void *)rom->as != (uintptr_t)(void *)item->as) ||
            (rom->as == item->as && rom->addr >= item->addr);
 }
 
